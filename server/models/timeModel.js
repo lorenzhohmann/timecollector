7e591e8de50cea36ee913be664afdece250382ea
unknown <hohmann.lorenz99@gmail.com>
2019-03-10 14:28:05 +0100

    second try initial commit

--- server/models/timeModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = mongoose.Schema({
-	'date': Date,
-	'from': Date,
-	'to': Date,
-	'break_from': Date,
-	'break_to': Date
-});
--- server/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Time = require('./timeModel.js');
-
-const userSchema = mongoose.Schema({
-	'name': String,
-	'times': [Time]
-});
-
-module.exports = mongoose.model('User', userSchema);

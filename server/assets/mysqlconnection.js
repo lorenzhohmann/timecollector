1ddb2819270fbc7f27c39b92d5dd52a164c6c9ff
LH <hohmann.lorenz99@gmail.com>
2021-11-26 15:28:12 +0100

    prepared for github

--- server/assets/mysqlconnection.js
+++ server/assets/mysqlconnection.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-	host: process.env.DB_HOST || 'localhost',
-	user: process.env.DB_USER || 'timecollector',
-	database: process.env.DB_DATABASE || 'timecollector',
-	password: process.env.DB_PASSWORD || 'B3bwuf^7VeU6uakb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
 });
 
 connection.connect();
